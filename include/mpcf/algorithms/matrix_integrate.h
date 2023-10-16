a54506af7da3b0a09ffe501fff4cc83aec0569e5
Bjorn Wehlin <bwehlin@kth.se>
2023-10-16 16:44:41 +0200

    Symmetric matrix on CPU

--- include/mpcf/algorithms/matrix_integrate.h
+++ include/mpcf/algorithms/matrix_integrate.h
@@ -19,15 +19,13 @@ namespace mpcf
   };
   
   template <typename Tt, typename Tv, typename RectangleOp>
-  Tv integrate(const Pcf<Tt, Tv>& f, const Pcf<Tt, Tv>& g, RectangleOp op, Tt a = 0.f, Tt b = std::numeric_limits<Tt>::max(), bool print=false)
+  Tv integrate(const Pcf<Tt, Tv>& f, const Pcf<Tt, Tv>& g, RectangleOp op, Tt a = 0.f, Tt b = std::numeric_limits<Tt>::max())
   {
     using rect_t = typename Pcf<Tt, Tv>::rectangle_type;
 
     Tv val = 0.f;
-    iterate_rectangles(f, g, a, b, [&val, &op, print](const rect_t& rect) -> void {
+    iterate_rectangles(f, g, a, b, [&val, &op](const rect_t& rect) -> void {
       val += (rect.right - rect.left) * op(rect);
-      if (print)
-        std::cout << rect.right - rect.left << " " << op(rect) << '\n';
     });
     
     return val;
@@ -39,10 +37,23 @@ namespace mpcf
     auto sz = fs.size();
     for (auto i = 0ul; i < sz; ++i)
     {
-      auto j = symmetric ? i : 0ul;
+      auto j = symmetric ? i : 0ul; // For now, fill entire matrix
       for (; j < sz; ++j)
       {
         out[i * sz + j] = integrate<Tt, Tv, RectangleOp>(fs[i], fs[j], op, a, b);
+      }
+    }
+    
+    if (symmetric)
+    {
+      // Build lower triangle
+      size_t sz = fs.size();
+      for (size_t i = 1; i < sz; ++i)
+      {
+        for (size_t j = 0; j < i; ++j)
+        {
+          out[i * sz + j] = out[j * sz + i];
+        }
       }
     }
   }
