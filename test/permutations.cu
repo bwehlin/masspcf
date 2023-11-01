36f9de10fced1cf7f571215ad94ca13ea490a11e
bwehlin <bwehlin@kth.se>
2023-11-01 16:24:11 +0100

    Added test

--- test/permutations.cu
+++ test/permutations.cu
@@ -32,3 +32,13 @@ TEST(Permutations, TwoCycles)
   EXPECT_EQ(cycles[0], std::vector<size_t>({ 1, 2 }));
   EXPECT_EQ(cycles[1], std::vector<size_t>({ 3, 4, 6 }));
 }
+
+TEST(Permutations, TwoCyclesWithSpace)
+{
+  std::vector<size_t> perm{ 0,2,1,3,4,6,5,7 };
+  auto cycles = mpcf::get_cycles(perm);
+  ASSERT_EQ(cycles.size(), 2ul);
+  EXPECT_EQ(cycles[0], std::vector<size_t>({ 1, 2 }));
+  EXPECT_EQ(cycles[1], std::vector<size_t>({ 5, 6 }));
+}
+
