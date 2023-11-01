3fb332c167c4665d0c3c621cffa6226ebc636039
bwehlin <bwehlin@kth.se>
2023-11-01 16:22:31 +0100

    Get permutation cycles

--- include/mpcf/algorithms/permute_in_place.h
+++ include/mpcf/algorithms/permute_in_place.h
@@ -5,6 +5,42 @@
 
 namespace mpcf
 {
+  std::vector<std::vector<size_t>> get_cycles(std::vector<size_t> permutation)
+  {
+    auto seen = permutation.size(); // Sentinel value
+    std::vector<std::vector<size_t>> cycles;
+
+    std::vector<size_t> cycle;
+    for (size_t i = 0ul; i < permutation.size(); ++i)
+    {
+      if (permutation[i] == seen) 
+      {
+        continue;
+      }
+
+      cycle.clear();
+      cycle.emplace_back(i);
+      auto n = permutation[i];
+      while (n != seen && n != i)
+      {
+        cycle.emplace_back(n);
+
+        auto nextn = permutation[n];
+        permutation[n] = seen;
+
+        n = nextn;
+      }
+
+      if (cycle.size() > 1)
+      {
+        cycles.emplace_back();
+        cycles.back().swap(cycle);
+      }
+    }
+
+    return cycles;
+  }
+
   template <typename Tv>
   void permute_in_place(Tv* matrix, const std::vector<size_t>& permutation)
   {
---
+++ test/permutations.cu
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+
+#include <mpcf/algorithms/permute_in_place.h>
+
+TEST(Permutations, EmptyPermutationHasNoCycles)
+{
+  std::vector<size_t> perm;
+  auto cycles = mpcf::get_cycles(perm);
+  EXPECT_TRUE(cycles.empty());
+}
+
+TEST(Permutations, SortedPermutationHasNoCycles)
+{
+  std::vector<size_t> perm{0,1,2,3};
+  auto cycles = mpcf::get_cycles(perm);
+  EXPECT_TRUE(cycles.empty());
+}
+
+TEST(Permutations, Cycle12)
+{
+  std::vector<size_t> perm{ 0,2,1,3 };
+  auto cycles = mpcf::get_cycles(perm);
+  ASSERT_EQ(cycles.size(), 1ul);
+  EXPECT_EQ(cycles[0], std::vector<size_t>({ 1, 2 }));
+}
+
+TEST(Permutations, TwoCycles)
+{
+  std::vector<size_t> perm{ 0,2,1,4,6,5,3 };
+  auto cycles = mpcf::get_cycles(perm);
+  ASSERT_EQ(cycles.size(), 2ul);
+  EXPECT_EQ(cycles[0], std::vector<size_t>({ 1, 2 }));
+  EXPECT_EQ(cycles[1], std::vector<size_t>({ 3, 4, 6 }));
+}
