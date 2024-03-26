d997cb5eb1ac5ae680776a8492f700366fedb5cf
bwehlin <bwehlin@kth.se>
2024-03-26 21:46:53 +0100

    example stuff

--- examples/arrays.py
+++ examples/arrays.py
@@ -1,12 +1,18 @@
 import masspcf as mpcf
 
-z = mpcf.zeros((3, 4,5))
+z = mpcf.zeros((3, 4, 6, 7))
 print(z.shape())
 
 #print(z[:,:2])
-v = z[0,1:3,:-2]
+v = z[0, 1:3, :, :]
+
+
 print(v.shape())
-v1 = v[0,:]
+print(v.data)
+v1 = v[0,1:3, :]
 print(v1.shape())
+print(v1.data)
+v2 = v1[1:2, :]
+print(v2.data)
 
 #print(v)
