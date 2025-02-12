4c1903405c316aa70c03be39f2f09c90afacdda8
Bjorn Wehlin <bwehlin@kth.se>
2025-02-12 11:29:18 +0100

    Build fixes

--- setup.py
+++ setup.py
@@ -3,13 +3,9 @@ from skbuild import setup
 setup(
     name='masspcf',
     version='0.3.1',
-    description='Massively parallel computations for piecewise constant functions',
     author='Bjorn H. Wehlin',
     license='TBD',
     packages=['masspcf'],
     python_requires=">=3.9",
-    cmake_args=['-DCMAKE_BUILD_TYPE=Release'],
-    install_requires=[
-        'numpy', 'numba', 'tqdm'
-    ]
+    cmake_args=['-DCMAKE_BUILD_TYPE=Release']
 )
