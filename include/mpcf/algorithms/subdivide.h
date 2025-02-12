2cad28e8fa5612942377c26ba72dff21ac801e8c
Bjorn Wehlin <bwehlin@kth.se>
2025-02-12 14:12:19 +0100

    New attempt at Windows fix

--- include/mpcf/algorithms/subdivide.h
+++ include/mpcf/algorithms/subdivide.h
@@ -34,7 +34,7 @@ namespace mpcf
         return boundaries;
       }
       
-      boundaries.emplace_back(i, std::min(i + blockSize - 1ul, nItems));
+      boundaries.emplace_back(i, std::min(i + blockSize - std::size_t(1), nItems));
       if (boundaries.back().second >= nItems)
       {
         boundaries.back().second = nItems - 1;
