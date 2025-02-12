65c2de2ca7d3fc29bcd44949d78246dd3e8386c8
Bjorn Wehlin <bwehlin@kth.se>
2025-02-12 13:41:48 +0100

    Copyright update

--- masspcf/matrix_computations.py
+++ masspcf/matrix_computations.py
@@ -1,4 +1,4 @@
-#    Copyright 2024 Bjorn Wehlin
+#    Copyright 2024-2025 Bjorn Wehlin
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
