c28d9cab2e8d3fefd7e2e1d61b954d4fdaaef7c3
Bjorn Wehlin <bjornw@Bjorns-MacBook-Pro.local>
2025-02-12 23:39:04 +0100

    Try getting CPU packages to work with actions

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from skbuild import setup
+from os import environ
+
+name_suffix='-cpu'
+
+if environ.get('BUILD_WITH_CUDA') is not None and environ.get('BUILD_WITH_CUDA') != '0':
+    name_suffix=''
 
 setup(
-    name='masspcf',
-    version='0.3.1',
+    name=f'masspcf{name_suffix}',
+    version='0.3.2',
     author='Bjorn H. Wehlin',
     license='TBD',
     packages=['masspcf'],
