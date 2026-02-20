ed61de6f72d888488f27c44101f0de8e6d185acb
Bjorn Wehlin <bwehlin@kth.se>
2026-02-20 10:32:09 +0100

    Try to fix toml reading without dependencies

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from skbuild import setup
 from os import environ
+import tomli
 
 name_suffix='-cpu'
+
+version = tomli.load(open('${CMAKE_CURRENT_SOURCE_DIR}/pyproject.toml', 'rb'))['project']['version']
 
 if environ.get('BUILD_WITH_CUDA') is not None and environ.get('BUILD_WITH_CUDA') != '0':
     name_suffix=''
 
 setup(
     name=f'masspcf{name_suffix}',
-    version='0.3.2',
+    version=version,
     author='Bjorn H. Wehlin',
-    license='TBD',
+    license='Apache2',
     packages=['masspcf'],
     python_requires=">=3.10",
     cmake_args=['-DCMAKE_BUILD_TYPE=Release']
